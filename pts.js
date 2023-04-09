5794bac41b217090fa1082a26147b4728ffd7162
NinaDicusar <119105328+NinaDicusar@users.noreply.github.com>
2023-04-09 20:59:37 +0300

    Add files via upload

---
+++ pts.js
@@ -0,0 +1,16 @@
+
+//function produce(){
+ //   return 1000
+//}
+function getRandomInt() {
+    return Math.floor(Math.random() * (10 - 0 + 1));
+    }
+
+function transform (p) {
+   let res = Math.floor(Math.random() / (10 - 0 + 1));
+ return res
+}
+
+function consume (p) {
+    result.innerHTML = ("p=" (Math.random() / (10 - 0 + 1)));
+}
